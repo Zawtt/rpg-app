1774d9ce34d00043e81dae308da2a08bf340f1cf
Zawtt <zawtofc.3594@gmail.com>
2025-07-23 03:50:11 -0300

    Primeira configuração do projeto React com Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 flex items-center justify-center text-white text-4xl font-bold">
+      Olá, Mundo RPG!
+    </div>
+  )
+}
+
+export default App
